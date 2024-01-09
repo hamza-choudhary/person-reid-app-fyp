c7b13bcf1d230e6b80a5ef00e176bc1bd9ca8540
hamza-choudhary <humzach700@gmail.com>
2024-01-09 16:54:55 +0500

    carsol added on result and query

--- frontend/src/components/Carosal/Carosal.tsx
+++ frontend/src/components/Carosal/Carosal.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
+type QueryItem = {
+  _id: string
+  image: string
+  name: string
+  description?: string
+  createdAt?: string
+}
+
 interface CarosalProps {
   setShowCarosal: (showCarosal: boolean) => void
   showCarosal: boolean
   imageType: string
-  images: string[]
+  images: string[] | QueryItem[]
   firstIndex: number
 }
 
@@ -35,63 +43,47 @@ const Carosal: React.FC<CarosalProps> = ({
   }
 
   if (!showCarosal) return null
+  if (
+    imageType !== 'query' &&
+    imageType !== 'gallery' &&
+    imageType !== 'results'
+  )
+    return null
 
   return createPortal(
     <>
       <div
-        className="flex justify-center items-center overflow-x-hidden fixed inset-0 z-[1200]"
+        className="flex justify-center items-center overflow-x-hidden fixed inset-0 z-[1203]"
         onClick={() => setShowCarosal(false)}
       >
         <div className="relative"></div>
+        {/* Image */}
         <div className="z-[1201]" onClick={(e) => e.stopPropagation()}>
-          <img
-            className="object-cover object-cente max-h-[90vh] max-w-[80vw]"
-            src={`http://localhost:8080/uploads/${imageType}/${images[currentIndex]}`}
-            alt="Carousel Image"
-          />
+          {imageType === 'query' ? (
+            <QueryCarosal query={images[currentIndex] as QueryItem} />
+          ) : (
+            <img
+              className="object-contain object-center max-h-[95vh] max-w-[80vw]"
+              src={`http://localhost:8080/uploads/${imageType}/${images[currentIndex]}`}
+              alt="Carousel Image"
+            />
+          )}
         </div>
-
+        {/* Buttons */}
         <div
-          className="absolute z-[1204] w-[50vw] h-auto flex justify-between items-center"
+          className="absolute z-[1204] w-[85vw] h-auto flex justify-between items-center"
           onClick={(e) => e.stopPropagation()}
         >
-          <button
-            className="p-2 rounded-full opacity-30 bg-black"
-            onClick={goToPrevious}
-          >
-            <svg
-              className="w-8 h-8 text-white"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M15 19l-7-7 7-7"></path>
-            </svg>
-          </button>
-
-          <button
-            onClick={goToNext}
-            className="p-2 rounded-full opacity-30 bg-black"
-          >
-            <svg
-              className="w-8 h-8 text-white"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M9 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <CarosalButton action="back" goTo={goToPrevious} />
+          <CarosalButton action="next" goTo={goToNext} />
         </div>
 
         {/* Background overlay */}
-        <div className="opacity-25 fixed inset-0 bg-black"></div>
+        <div
+          className={`${
+            imageType === 'query' ? 'opacity-90' : 'opacity-20'
+          } fixed inset-0 bg-black`}
+        ></div>
       </div>
       ,
     </>,
@@ -100,3 +92,74 @@ const Carosal: React.FC<CarosalProps> = ({
 }
 
 export default Carosal
+
+interface ForwardButtonProps {
+  goTo: () => void
+  action: string
+}
+
+function CarosalButton({ goTo, action }: ForwardButtonProps) {
+  if (action !== 'next' && action !== 'back') {
+    return null
+  }
+  return (
+    <button onClick={goTo} className="p-2 rounded-full opacity-90 bg-slate-500">
+      {action === 'next' && (
+        <svg
+          className="w-8 h-8 text-white"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M9 5l7 7-7 7"></path>
+        </svg>
+      )}
+
+      {action === 'back' && (
+        <svg
+          className="w-8 h-8 text-white"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M15 19l-7-7 7-7"></path>
+        </svg>
+      )}
+    </button>
+  )
+}
+
+function QueryCarosal({ query }: { query: QueryItem }) {
+  return (
+    <div className="flex justify-center">
+      <img
+        className="object-contain object-right max-h-[95vh] max-w-[60rem]"
+        src={`http://localhost:8080/uploads/query/${query.image}`}
+        alt="Carousel Image"
+      />
+      <div className="flex flex-col pl-4 w-[25rem] bg-zinc-400 bg-opacity-40 text-white">
+        <h2 className="font-semibold text-lg">Name: </h2>
+        <p>{query.name}</p>
+
+        {query.createdAt && (
+          <>
+            <h2 className="font-semibold text-lg">Created At: </h2>
+            <p>{query.createdAt}</p>
+          </>
+        )}
+        {query.description && (
+          <>
+            <h2 className="font-semibold text-lg">Description: </h2>
+            <p className="overflow-y-auto max-h-[60vh]">{query.description}</p>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/Query/index.tsx
+++ frontend/src/pages/Query/index.tsx
@@ -4,6 +4,7 @@ import Modal from '../../components/Modal/Modal'
 import ResponsiveDrawer from '../../components/Sidebar'
 import Button from '../../components/UI/Button'
 import DragAndDrop from '../../components/UI/DragAndDrop'
+import Carosal from '../../components/Carosal/Carosal'
 
 //FIXME: add modal for description
 
@@ -26,6 +27,14 @@ export default function QueryPage() {
 		name: '',
 		description: '',
 	})
+
+	const [showCarosal, setShowCarosal] = useState(false)
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const openCarosal = (index: number) => {
+    setImageIndex(index)
+    setShowCarosal(true)
+  }
 
 	const handleChange = (
 		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -146,7 +155,7 @@ export default function QueryPage() {
 				<div className="columns-1 md:gap-2 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 ">
 					{queries &&
 						queries.map((query, index) => (
-							<div key={query._id} className="relative cursor-pointer">
+							<div key={query._id} className="relative cursor-pointer" onClick={()=>openCarosal(index)}>
 								<img
 									className={`object-top object-cover rounded-sm max-h-[30rem] ${
 										index !== 0 ? 'mt-2' : ''
@@ -188,7 +197,7 @@ export default function QueryPage() {
           setShowCarosal={setShowCarosal}
           firstIndex={imageIndex}
           imageType="query"
-          images={}
+          images={queries}
         />
 				<Modal
 					showModal={showAddQueryModal}
