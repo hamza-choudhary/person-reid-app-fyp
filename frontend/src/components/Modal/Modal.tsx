d690d96b9af6f50b0995dcba1374fcc9b5355de1
hamza-choudhary <humzach700@gmail.com>
2024-01-11 03:04:17 +0500

    modal fixed

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -19,20 +19,22 @@ const Modal: FC<ModalProps> = ({
 	return createPortal(
 		<>
 			<div
-				className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[9999] outline-none focus:outline-none"
+				className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-[1203] outline-none focus:outline-none"
+				onClick={() => setShowModal(false)}
 			>
-				<div className={`relative w-auto my-6 mx-auto max-w-3xl ${className}  max-h-[90vh]`}>
+				<div
+					className={`my-6 mx-auto max-w-3xl max-h-[90vh] ${className} z-[1201]`}
+					onClick={(e) => e.stopPropagation()}
+				>
 					{/* card */}
-					<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+					<div className="border-0 rounded-lg shadow-lg flex flex-col w-full bg-white outline-none focus:outline-none">
 						{/* content */}
 						{children}
 					</div>
 				</div>
+
+				<div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
 			</div>
-			<div
-				onClick={() => setShowModal(false)}
-				className="opacity-25 fixed inset-0 z-40 bg-black"
-			></div>
 		</>,
 		document.getElementById('portal-root') as HTMLElement
 	)
